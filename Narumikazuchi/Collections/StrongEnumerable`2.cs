fc892f4bd787eff1fbb94da5afd9d7980535e9d9
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-02-22 17:21:35 +0100

    Added base class for strong enumerables.

---
+++ Narumikazuchi/Collections/StrongEnumerable`2.cs
@@ -0,0 +1,23 @@
+﻿namespace Narumikazuchi.Collections;
+
+/// <summary>
+/// Represents the base class for an <see cref="IStrongEnumerable{TElement, TEnumerator}"/>.
+/// </summary>
+public abstract class StrongEnumerable<TElement, TEnumerator> : IStrongEnumerable<TElement, TEnumerator>
+    where TEnumerator : struct, IStrongEnumerator<TElement>
+{
+    /// <inheritdoc/>
+    public abstract TEnumerator GetEnumerator();
+
+#if !NETCOREAPP3_1_OR_GREATER
+    IEnumerator<TElement> IEnumerable<TElement>.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+#endif
+}
